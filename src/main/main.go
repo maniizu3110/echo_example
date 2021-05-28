8d0ca33fb99e7573dba7e91c93535fabea61f901
Leo <49385552+maniizu3110@users.noreply.github.com>
2021-05-28 09:22:53 +0900

    end comparing echo and native go

--- src/main/main.go
+++ src/main/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -10,24 +9,35 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type User struct {
+	Name string `json:"name"`
+}
+type Dog struct {
+	Name string `json:"name"`
+}
+type Cat struct {
+	Name string `json:"name"`
+}
+
 func main() {
 	e := echo.New()
 	e.GET("/", yallo)
-	e.POST("/add", addUser)
-	e.GET("/user/:id", getQuery)
+	e.POST("user/add", addUser)
+	e.POST("dog/add", addDog)
+	e.POST("cat/add", addCat)
+	e.GET("/user/:data", getQuery)
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
 func yallo(c echo.Context) error {
 	return c.String(http.StatusOK, "yallo")
 }
+
 func getQuery(c echo.Context) error {
-	// http://localhost:8080/user/3?name=leoで接続すると値を返す
+	// http://localhost:8080/user/json?name=leoで接続すると値を返す
 	name := c.QueryParam("name")
-	id := c.Param("id")
 	dataType := c.Param("data")
 
-	fmt.Println(id)
 	if dataType == "strign" {
 		return c.String(http.StatusOK, name)
 	}
@@ -40,25 +50,47 @@ func getQuery(c echo.Context) error {
 	return c.JSON(http.StatusBadRequest, map[string]string{"error": "internal server error"})
 }
 
-type User struct {
-	Name string
-}
-
+//Native go1
 func addUser(c echo.Context) error {
 	user := User{}
 	defer c.Request().Body.Close()
 	b, err := ioutil.ReadAll(c.Request().Body)
 	if err != nil {
-		log.Printf("failed loading the request body for add")
+		log.Printf("failed loading the request body for add: %s", err)
 		return c.String(http.StatusInternalServerError, "")
 	}
 	err = json.Unmarshal(b, &user)
-	log.Printf(err.Error())
 	if err != nil {
-		log.Printf("failed unmardshaling in add")
+		log.Printf("failed unmarsharing: %s", err)
 		return c.String(http.StatusInternalServerError, "")
 	}
-	log.Printf("this is your user:%v", user)
+	log.Printf("this is your user:%#v", user)
 	return c.String(http.StatusOK, "we got your user")
 
+}
+
+//Native go2
+func addDog(c echo.Context) error {
+	dog := Dog{}
+
+	defer c.Request().Body.Close()
+	err := json.NewDecoder(c.Request().Body).Decode(&dog)
+	if err != nil {
+		log.Printf("failed processing addDog request: %s", err)
+		return c.String(http.StatusInternalServerError, "")
+	}
+	log.Printf("this is your user:%#v", dog)
+	return c.String(http.StatusOK, "we got your dog")
+}
+
+//Using Echo
+func addCat(c echo.Context) error {
+	cat := Cat{}
+	err := c.Bind(&cat)
+	if err != nil {
+		log.Printf("Failed processing cat request: %s", err)
+		return echo.NewHTTPError(http.StatusInternalServerError)
+	}
+	log.Printf("this is your cat:%#v", cat)
+	return c.String(http.StatusOK, "we get your cat")
 }
