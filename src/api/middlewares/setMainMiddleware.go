471afb69f812ed9a53a9f5351f79cdc51af3a962
Leo <49385552+maniizu3110@users.noreply.github.com>
2021-06-01 07:42:08 +0900

    corsに対応

--- src/api/middlewares/setMainMiddleware.go
+++ src/api/middlewares/setMainMiddleware.go
@@ -1,11 +1,18 @@
 package middlewares
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func SetMainMiddlewares(e *echo.Echo) {
 	e.Use(serverHeader)
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+        AllowOrigins: []string{"http://localhost:3000", "https://next-redux-app-tau.vercel.app/"},
+        AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
+    }))
 }
 
 func serverHeader(next echo.HandlerFunc) echo.HandlerFunc {
