1c7ee1a8e0213635675ed1657e1fc784e4852485
Leo <49385552+maniizu3110@users.noreply.github.com>
2021-06-03 14:21:03 +0900

    cors関数を開発環境と合わせる

--- src/api/middlewares/setMainMiddleware.go
+++ src/api/middlewares/setMainMiddleware.go
@@ -6,11 +6,12 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
+
 //CorsMiddlewares corsエラーを避ける
 func CorsMiddlewares(e *echo.Echo) {
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins:     []string{"http://localhost:3000", "https://next-redux-app-tau.vercel.app/"},
-		AllowMethods:     []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete, echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+		AllowMethods:     []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
 		AllowCredentials: true,
 	}))
 }
