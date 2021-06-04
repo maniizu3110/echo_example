e455f805248a109eb7e1b247334efa761a10ac5d
Leo <49385552+maniizu3110@users.noreply.github.com>
2021-06-04 16:06:53 +0900

    Get実装完了

--- src/api/handlers/user_handler.go
+++ src/api/handlers/user_handler.go
@@ -5,18 +5,19 @@ import (
 	"myapp/src/api/models"
 	"myapp/src/db"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
 
 //UserHandler retreave /user
-func UserHandler(g *echo.Group){
-	g.POST("",createHandler)
-	g.GET("",getAll)
+func UserHandler(g *echo.Group) {
+	g.POST("", createHandler)
+	g.GET("", getAll)
+	g.GET("/:id", get)
 }
 
-
-func createHandler(c echo.Context)(err error){
+func createHandler(c echo.Context) (err error) {
 	//TODO:共通化（db2回呼んでいる）
 	db := db.InitDB()
 	user := new(models.User)
@@ -33,4 +34,17 @@ func getAll(c echo.Context) error {
 	fmt.Println(result)
 
 	return c.String(http.StatusOK, "get Users")
+}
+
+func get(c echo.Context) (err error ){
+	db := db.InitDB()
+	data:= &models.User{}
+	id, err := strconv.ParseUint(c.Param("id"), 10, 64)
+	if err != nil {
+		return err
+	}
+	result := db.Where("id = ?",id).First(data)
+	fmt.Println(result)
+
+	return c.JSON(http.StatusOK,data)
 }
