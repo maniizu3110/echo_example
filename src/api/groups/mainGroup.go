190096fbfc92dc6a311da2306a3ba61297942128
Leo <49385552+maniizu3110@users.noreply.github.com>
2021-06-04 09:23:02 +0900

    いらないファイルの削除

--- src/api/groups/mainGroup.go
+++ src/api/groups/mainGroup.go
@@ -6,10 +6,5 @@ import (
 )
 
 func MainGroup(e *echo.Echo) {
-	e.GET("/login", handlers.Login)
 	e.POST("/user", handlers.CreateUser)
-	e.GET("/cats/:data", handlers.GetCats)
-	e.POST("/cats", handlers.AddCat)
-	// e.POST("/dogs", handlers.AddDog)
-	e.POST("/hamsters", handlers.AddHamster)
 }
--- src/router/router.go
+++ src/router/router.go
@@ -10,27 +10,12 @@ import (
 func Run() *echo.Echo {
 	e := echo.New()
 
-	// create groups
-	//api/v1をグループ化しておきたい
 	g := e.Group("/api/v1")
-	adminGroup := e.Group("/admin")
-	cookieGroup := e.Group("/cookie")
-	jwtGroup := e.Group("/jwt")
 
-	// set all middlewares
 	middlewares.CorsMiddlewares(e) //何もしてないやつ（グループの練習）
 	middlewares.CheckAdminMiddlewares(g)
-	// middlewares.SetAdminMiddlewares(adminGroup)
-	middlewares.SetCookieMiddlewares(cookieGroup)
-	middlewares.SetJwtMiddlewares(jwtGroup)
 
-	// set main routes
 	api.MainGroup(e)
-	// set group routes
-	api.AdminGroup(adminGroup)
-	api.CookieGroup(cookieGroup)
-	//jwt認証がないと通れないということは全てのurlがこれを通るということ
-	api.JwtGroup(jwtGroup)
 
 	return e
 }
