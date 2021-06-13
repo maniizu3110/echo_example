7124114e871c2d6a2e83d7d170c305790efc8d2a
Leo <49385552+maniizu3110@users.noreply.github.com>
2021-06-13 13:39:59 +0900

    main.goをnginxを使わない形に変更 (#59)

--- src/main/main.go
+++ src/main/main.go
@@ -3,18 +3,12 @@ package main
 import (
 	"myapp/src/db"
 	"myapp/src/router"
-	"net"
-
 	"github.com/jinzhu/gorm"
 
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func main() {
-	_, err := net.Listen("tcp", "127.0.0.1:8000")
-    if err != nil {
-        return
-    }
 	var database *gorm.DB
 	database = db.InitDB()
 	defer database.Close()
